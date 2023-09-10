4d5dc33a097cfd72b983f95700d387f691bc8d97
Lily <144317528+Lily-lena@users.noreply.github.com>
2023-09-10 19:39:28 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,35 @@
 let Countdown = 0
 let Text_2 = 0
-let Text_angezeigt = 0
 let knopf_a_counter = 0
-if (Text_angezeigt == 0) {
-    if (!(input.buttonIsPressed(Button.A))) {
-        basic.showString("Zum Start: drücke Knopf A")
-        Text_angezeigt += 1
+let Text_angezeigt = 0
+let Start = 0
+Start += storage.getNumber(StorageSlots.s1)
+if (Start == 0) {
+    Start += 1
+    storage.putNumber(StorageSlots.s1, Start)
+    Text_angezeigt = 0
+    knopf_a_counter = 0
+    if (Text_angezeigt == 0) {
+        if (!(input.buttonIsPressed(Button.A))) {
+            basic.showString("Zum Start: drücke A")
+            Text_angezeigt += 1
+        } else {
+            knopf_a_counter += 1
+        }
     } else {
-        knopf_a_counter += 1
+        control.waitForEvent(knopf_a_counter, 1)
     }
+} else if (Start == 1 || Start == 2) {
+    Start += 1
+    storage.putNumber(StorageSlots.s1, Start)
 } else {
-    control.waitForEvent(knopf_a_counter, 1)
+    storage.removeNumber(StorageSlots.s1)
 }
 basic.forever(function () {
     if (!(input.isGesture(Gesture.ScreenUp)) && Text_2 == 0) {
         basic.showString("Nicht gerade")
         basic.setLedColor(0xff0000)
-        basic.showString("Neige leicht")
+        basic.showString("Neige wie Pfeile")
         Text_2 += 1
         if (input.isGesture(Gesture.Shake)) {
             basic.showString("ruhig halten")
@@ -82,7 +95,7 @@ basic.forever(function () {
             }
         }
     } else if (!(input.isGesture(Gesture.ScreenUp)) && Text_2 > 0) {
-        basic.showString("Folge Pfeilen")
+        basic.showString("Bewege leicht")
         if (input.isGesture(Gesture.Shake)) {
             basic.showString("ruhig halten")
         }
@@ -159,6 +172,7 @@ basic.forever(function () {
         basic.showString("halte Position")
         Countdown = 5
         basic.showNumber(Countdown)
+        basic.pause(200)
         for (let index = 0; index < 5; index++) {
             if (input.isGesture(Gesture.ScreenUp)) {
                 Countdown += -1
